d3e2d6c0c63d4d10873612dae2255cc0c8df78e4
AlexisCangelosi <cangelosi.alexis@gmail.com>
2016-07-06 14:17:27 +0200

    add x509 client

---
+++ S5/EVAL/fin_semestre/VPN/x509/add_x509_client.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+###############################################################################
+# Auteur : Alexis Cangelosi
+# Promotion : ITI14M SR
+# Theme : Evaluation de fin de semestre
+# Titre : Création x509 client [AUTOMATISATION]
+###############################################################################
+
+###############################################################################
+# 								VARIABLES									  
+###############################################################################
+
+root_vpn="/opt/vpn/x509"
+root_openvpn="/etc/openvpn"
+root_install="/usr/share/easy-rsa/"
+conf_name="serveur_x509.conf"
+client_name="None"
+server_port="None"
+
+###############################################################################
+# 								SCRIPT									  
+###############################################################################
+
+# Clean sheet
+clear
+
+echo "###########################################################################"
+echo "Création d'un client vpn"
+echo "###########################################################################"
+echo ""
+echo "[*] Nom du client :"
+read -p "[*] -> " client_name
+echo "[*] Port :"
+read -p "[*] -> " server_port
+
+mkdir -p $root_vpn/client/$client_name
+
+cd $root_install
+source vars
+$root_install/build-key $client_name
+
+cp $root_install/keys/ca.crt $root_vpn/client/$client_name/
+cp $root_install/keys/$client_name.* $root_vpn/client/$client_name/
+
+echo "[*] Creation du fichier de configuration client"
+echo "
+client
+dev tun
+proto udp
+
+remote labo.itinet.fr
+port $server_port
+
+resolv-retry infinite
+nobind
+
+persist-key
+persist-tun
+
+ca ca.crt
+cert $client_name.crt
+key $client_name.key
+
+comp-lzo
+
+verb 6
+" > $root_vpn/client/$client_name/$client_name.conf
+
+exit 0
