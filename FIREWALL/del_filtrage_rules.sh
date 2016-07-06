1d7861e3376602579aa2179fad15a3a2f0202f6c
AlexisCangelosi <cangelosi.alexis@gmail.com>
2016-07-06 22:14:29 +0200

    del rules

---
+++ FIREWALL/del_filtrage_rules.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+###############################################################################
+# Auteur : Alexis Cangelosi
+# Promotion : ITI14M SR
+# Theme : Evaluation de fin de semestre
+# Titre : Del des regles de filtrage
+###############################################################################
+
+###############################################################################
+# 								VARIABLES									  
+###############################################################################
+
+path="/opt/firewall"
+bash="#!/bin/bash \n"
+
+ip_src="None"
+ip_dst="None"
+port_dst="None"
+port_src="None"
+proto="None"
+type_filt="None"
+rule="None"
+
+###############################################################################
+# 								SCRIPT									  
+###############################################################################
+
+# Clean sheet
+clear
+
+echo "###########################################################################"
+echo "MISE EN PLACE DES REGLES DE FILTRAGE !"
+echo "###########################################################################"
+echo "[*] Type : "
+echo "[1] INPUT "
+echo "[2] OUTPUT "
+echo "[3] FORWARD "
+read -p "[*] => " type_filt
+echo "[*] IP source : (default = None)"
+read -p "[*] => "
+echo "[*] IP destination : (default = None)"
+read -p "[*] => "
+echo "[*] Port source : (default = None)"
+read -p "[*] => "
+echo "[*] Port destionation : (default = None)"
+read -p "[*] => "
+
+if [ $proto -eq "udp" ] ; then
+	rule="iptables -A $type_filt -s $ip_src -d $ip_dst -p $proto --sport $port_src --dport $port_dst -m state --state NEW -j ACCEPT"
+elif [ $proto -eq "tcp" ] ; then
+	rule="iptables -A $type_filt -s $ip_src -d $ip_dst -p $proto --sport $port_src --dport $port_dst -m state --state NEW --syn -j ACCEPT"
+else 
+	echo "[!] Protocole non correct !"
+	./add_filtrage_rules.sh
+fi
+
+find $path/filtrage_in_progress.sh -type f -exec sed -i "s/$rule/ /g" {} \+
+
+.$path/default_firewall.sh
+.$path/filtrage_in_progress.sh
+.$path/redirect_in_progress.sh
+
+echo "[!] Règle supprimée :"
+echo $rule
---
+++ FIREWALL/del_redirect_rules.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+###############################################################################
+# Auteur : Alexis Cangelosi
+# Promotion : ITI14M SR
+# Theme : Evaluation de fin de semestre
+# Titre : Del Redirection règles
+###############################################################################
+
+###############################################################################
+# 								VARIABLES									  
+###############################################################################
+
+path="/opt/firewall"
+bash="#!/bin/bash \n"
+
+ip="None"
+port_dst_int="None"
+port_dst_ext="None"
+proto="None"
+rule_1="None"
+rule_2="None"
+
+###############################################################################
+# 								SCRIPT									  
+###############################################################################
+
+# Clean sheet
+clear
+
+echo "###########################################################################"
+echo "SUPPRESSION DE REGLE DE REDIRECTION !"
+echo "###########################################################################"
+echo "[*] Protocole : (tcp/udp) "
+read -p "[*] => " proto
+echo "[*] IP "
+read -p "[*] => " ip
+echo "[*] Port destination externe : "
+read -p "[*] => " port_dst_ext
+echo "[*] Port destination interne : "
+read -p "[*] => " port_dst_int
+
+if [ $proto -eq "udp" ] ; then
+	rule_1="iptables -t nat -A PREROUTING -i eth0 -p $proto --dport $port_dst_ext -j DNAT --to-destination $ip_dst:$port_dst_int"
+    rule_2="iptables -A FORWARD -i eth0 -o eth1 -p $proto --dport $port_dst_ext -m state --state NEW -j ACCEPT"
+
+elif [ $proto -eq "tcp" ] ; then
+	rule_1="iptables -t nat -A PREROUTING -i eth0 -p $proto --dport $port_dst_ext -j DNAT --to-destination $ip_dst:$port_dst_int"
+    rule_2="iptables -A FORWARD -i eth0 -o eth1 -p $proto --dport $port_dst_ext -m state --state NEW -j ACCEPT"
+
+else 
+	echo "[!] Protocole non correct !"
+	./add_redirect_rules.sh
+fi
+
+find $path/redirect_in_progress.sh -type f -exec sed -i "s/$rule_1/ /g" {} \+
+find $path/redirect_in_progress.sh -type f -exec sed -i "s/$rule_2/ /g" {} \+
+
+.$path/default_firewall.sh
+.$path/filtrage_in_progress.sh
+.$path/redirect_in_progress.sh
+
+echo "[!] Règles supprimées :"
+echo $rule_1
+echo $rule_2
