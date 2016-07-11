0cf84e8d91bacca461a5d300fcddbda0f153ef58
AlexisCangelosi <cangelosi.alexis@gmail.com>
2016-07-11 10:05:54 +0000

    add client

--- FIREWALL/redirect_in_progress.sh
+++ FIREWALL/redirect_in_progress.sh
@@ -1,1 +1,3 @@
 #!/bin/bash
+iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j DNAT --to-destination 192.168.0.100:5000
+iptables -A FORWARD -i eth0 -o eth1 -p tcp --dport 80 -m state --state NEW -j ACCEPT
